f7175267c8f9f4758b96901314f8c7d8f3e5063a
Cbunz <anderson.connor.d@gmail.com>
2018-08-12 20:18:05 -0700

    First Commit

---
+++ Shield Wield/Assets/Scripts/Damager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damager : MonoBehaviour
+{
+
+	[Serializable]
+    public class DamageableEvent : UnityEvent<Damager, Damageable>
+    {
+    }
+
+    [Serializable]
+    public class NonDamageableEvent : UnityEvent<Damager>
+    {
+    }
+
+    protected Collider2D lastHit;
+    public Collider2D LastHit { get { return lastHit; } }
+
+    public DamageableEvent OnDamageableHit;
+    public NonDamageableEvent OnNonDamageableHit;
+
+    public int damage = 1;
+
+    public SpriteRenderer spriteRenderer;
+    public LayerMask hittableLayers;
+
+    protected bool spriteOriginallyFlipped;
+    protected bool canDamage = true;
+    protected ContactFilter2D attackContactFilter;
+    protected Collider2D[] attackOverlapResults = new Collider2D[10];
+    protected Transform damagerTransform;
+
+    public Vector2 offset = new Vector2(1.5f, 1f);
+    public Vector2 size = new Vector2(2.5f, 1f);
+    public bool offsetBasedOnSpriteFacing = true;
+    
+    public bool canHitTriggers;
+    public bool disableDamageAfterHit = false;
+    public bool forceRespawn = false;
+    public bool ignoreInvincibility = false;
+
+    void Awake()
+    {
+        attackContactFilter.layerMask = hittableLayers;
+        attackContactFilter.useLayerMask = true;
+        attackContactFilter.useTriggers = canHitTriggers;
+
+        if (offsetBasedOnSpriteFacing && spriteRenderer != null)
+        {
+            spriteOriginallyFlipped = spriteRenderer.flipX;
+        }
+
+        damagerTransform = transform;
+    }
+
+    public void EnableDamage()
+    {
+        canDamage = true;
+    }
+
+    public void DisableDamage()
+    {
+        canDamage = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!canDamage)
+        {
+            return;
+        }
+
+        Vector2 scale = damagerTransform.lossyScale;
+
+        Vector2 facingOffset = Vector2.Scale(offset, scale);
+
+        if (offsetBasedOnSpriteFacing && spriteRenderer != null && spriteRenderer.flipX != spriteOriginallyFlipped)
+        {
+            facingOffset = new Vector2(-offset.x * scale.x, offset.y * scale.y);
+        }
+
+        Vector2 scaledSize = Vector2.Scale(size, scale);
+
+        Vector2 pointA = (Vector2)damagerTransform.position + facingOffset - scaledSize * 0.5f;
+        Vector2 pointB = pointA + scaledSize;
+
+        int hitCount = Physics2D.OverlapArea(pointA, pointB, attackContactFilter, attackOverlapResults);
+
+        for (int i = 0; i < hitCount; i++)
+        {
+            lastHit = attackOverlapResults[i];
+            Damageable damageable = lastHit.GetComponent<Damageable>();
+
+            if (damageable)
+            {
+                OnDamageableHit.Invoke(this, damageable);
+                damageable.TakeDamage(this, ignoreInvincibility);
+                if (disableDamageAfterHit)
+                {
+                    DisableDamage();
+                }
+            }
+            else
+            {
+                OnNonDamageableHit.Invoke(this);
+            }
+        }
+    }
+}
---
+++ Shield Wield/Assets/Scripts/PhysicsHelper.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PhysicsHelper : MonoBehaviour {
+
+    static PhysicsHelper instance;
+    static PhysicsHelper Instance
+    {
+        get
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            instance = FindObjectOfType<PhysicsHelper>();
+
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            Create();
+
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+
+    static void Create()
+    {
+        GameObject physicsHelperGameObject = new GameObject("PhysicsHelper");
+        instance = physicsHelperGameObject.AddComponent<PhysicsHelper>();
+    }
+
+    Dictionary<Collider2D, MovingPlatform> movingPlatformCache = new Dictionary<Collider2D, MovingPlatform>();
+    Dictionary<Collider2D, PlatformEffector2D> platformEffectorCache = new Dictionary<Collider2D, PlatformEffector2D>();
+    Dictionary<Collider2D, Tilemap> tilemapCache = new Dictionary<Collider2D, Tilemap>();
+
+    void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        PopulateColliderDictionary(movingPlatformCache);
+        PopulateColliderDictionary(platformEffectorCache);
+        PopulateColliderDictionary(tilemapCache);
+    }
+
+    protected void PopulateColliderDictionary<TComponent>(Dictionary<Collider2D, TComponent> dict)
+        where TComponent : Component
+    {
+        TComponent[] components = FindObjectsOfType<TComponent>();
+
+        for (int i = 0; i < components.Length; i++)
+        {
+            Collider2D[] componentColliders = components[i].GetComponents<Collider2D>();
+
+            for (int j = 0; j < componentColliders.Length; j++)
+            {
+                dict.Add(componentColliders[j], components[i]);
+            }
+        }
+    }
+
+    public static bool ColliderHasMovingPlatform(Collider2D collider)
+    {
+        return Instance.movingPlatformCache.ContainsKey(collider);
+    }
+    public static bool ColliderHasPlatformEffector(Collider2D collider)
+    {
+        return Instance.platformEffectorCache.ContainsKey(collider);
+    }
+    public static bool ColliderHasTilemap(Collider2D collider)
+    {
+        return Instance.tilemapCache.ContainsKey(collider);
+    }
+    public static bool TryGetMovingPlatform(Collider2D collider, out MovingPlatform movingPlatform)
+    {
+        return Instance.movingPlatformCache.TryGetValue(collider, out movingPlatform);
+    }
+    public static bool TryGetPlatformEffector(Collider2D collider, out PlatformEffector2D platformEffector)
+    {
+        return Instance.platformEffectorCache.TryGetValue(collider, out platformEffector);
+    }
+    public static bool TryGetTilemap(Collider2D collider, out Tilemap tilemap)
+    {
+        return Instance.tilemapCache.TryGetValue(collider, out tilemap);
+    }
+}
