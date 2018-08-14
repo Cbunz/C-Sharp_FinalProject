63c6a5970769f3f1e1498fa7bbfb7ff6749ff11a
Jlevine <fundragonstudios@gmail.com>
2018-08-13 22:11:34 -0700

    Jakes_AI_Import
    Important Jakes AI Scripts (Test)

---
+++ Shield Wield/Assets/Scripts/AttackingAI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingAI : MonoBehaviour
+{
+
+    public float enemySpeed;
+    public float distance;
+    public float engageSpeed;
+
+    Rigidbody2D theRigidBody;
+
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+    public Transform playerDetection;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * enemySpeed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        RaycastHit2D playerInfo = Physics2D.Raycast(playerDetection.position, Vector2.left, distance);
+        if (playerInfo.collider == true)
+        {
+            Engage();
+        }
+
+
+        else
+        {
+            {
+                if (groundInfo.collider == false)
+                {
+                    if (movingRight == true)
+                    {
+                        transform.eulerAngles = new Vector3(0, -180, 0);
+                        movingRight = false;
+
+                    }
+                    else
+                    {
+                        transform.eulerAngles = new Vector3(0, 0, 0);
+                        movingRight = true;
+                    }
+                }
+            }
+        }
+        
+
+
+
+    }
+
+    void Engage()
+    {
+        theRigidBody.AddForce(transform.forward * engageSpeed);
+        //transform.Translate(Vector3.forward * engageSpeed * Time.deltaTime);
+    }
+}
+
---
+++ Shield Wield/Assets/Scripts/MiddleLevelAI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour
+{
+
+    public float fpsTargetDistance;
+    public float enemyLookDistance;
+    public float engageDistance;
+    public float attackDistance;
+    public float enemyMovementSpeed;
+    public float damping;
+    public Transform fpsTarget;
+    Rigidbody theRigidbody;
+    Renderer myRender;
+    //Animator m_Animator;    //Eric's changes
+    private Animation anim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRender = GetComponent<Renderer>();
+        theRigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        if (fpsTargetDistance < enemyLookDistance)
+        {
+            lookAtPlayer();
+        }
+        if (fpsTargetDistance < engageDistance)
+        {
+            attackPlayer();
+            print("attack");
+        }
+    }
+
+    void lookAtPlayer()
+    {
+
+        //Quaternion rotation = Quaternion.LookRotation (fpsTarget.position - transform.position);
+        //    transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
+        transform.LookAt(new Vector3(fpsTarget.position.x, transform.position.y, fpsTarget.position.z), Vector3.up);
+
+    }
+    void attackPlayer()
+    {
+
+
+        if (fpsTargetDistance < attackDistance)
+        {
+            anim.Play("Attack_Right");
+        }
+
+        else
+        {
+            theRigidbody.AddForce(transform.forward * enemyMovementSpeed);
+        }
+    }
+}
