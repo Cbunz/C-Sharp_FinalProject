ea74bfe29c08bcb75e4ae5af49db53de56eef339
Jlevine <fundragonstudios@gmail.com>
2018-08-14 02:41:15 -0700

    Jake's Intial Commit
    AI Scripts, Boulder Trap, Level Designs, AI Script Edits, animations.

---
+++ Shield Wield/Assets/player_Enter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_Enter : MonoBehaviour {
+
+    public GameObject Stone;
+
+    private void OnTriggerEnter2D()
+    {
+        Stone.SetActive(true);
+    }
+}
