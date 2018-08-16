e478a0c77fe853b7b3439222e90bb93498bcf8f2
Cbunz <anderson.connor.d@gmail.com>
2018-08-16 13:33:24 -0700

    Added Flying Guys

---
+++ Shield Wield/Assets/Scripts/FlyingEnemyScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyScript : MonoBehaviour {
+
+    private CharacterController2D character;
+
+    public float speed;
+    public float range;
+    public LayerMask playerLayer;
+    public bool characterInRange;
+    public bool goPastEnemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = FindObjectOfType<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        characterInRange = Physics2D.OverlapCircle(transform.position, range, playerLayer);
+
+        if (character.transform.position.x > transform.position.x && character.transform.localScale.x > 0)
+        {
+            goPastEnemy = true;
+        }
+        else
+        {
+            goPastEnemy = false;
+        }
+
+        if (characterInRange && goPastEnemy)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, character.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+}
